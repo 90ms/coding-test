2d0ddbf90ff865c39043bdc2065a6f1a63ce873e
odin-lific <82076270+odin-lific@users.noreply.github.com>
2021-04-12 15:30:56 +0900

    신규 회원가입 규칙

--- app/src/main/java/com/example/coding_test/MainActivity.kt
+++ app/src/main/java/com/example/coding_test/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.coding_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coding_test.level_1.CreateId
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CreateId.solution("1111!@#$%^&*()_+-111")
     }
 }
---
+++ app/src/test/java/com/example/coding_test/CreateIdTest.kt
@@ -0,0 +1,19 @@
+package com.example.coding_test
+
+import com.example.coding_test.level_1.CreateId
+import org.junit.Assert
+import org.junit.Test
+
+class CreateIdTest {
+
+    @Test
+    fun solution1() {
+        val result = CreateId.solution(ANSWER_1)
+        Assert.assertEquals(RESULT_1, result)
+    }
+
+    companion object {
+        val ANSWER_1 = ".ABCd...1..2345!@#$%^&*()_+-.,12345."
+        val RESULT_1 = "abcd.1.2345_-.12345"
+    }
+}
