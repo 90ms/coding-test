672ca40b0511f0cdb3830ce91af5899951b1f9fe
odin-lific <82076270+odin-lific@users.noreply.github.com>
2021-04-12 15:44:05 +0900

    신규 회원가입 규칙(추가)

--- app/src/main/java/com/example/coding_test/level_1/CreateId.kt
+++ app/src/main/java/com/example/coding_test/level_1/CreateId.kt
@@ -174,4 +174,20 @@ object CreateId {
 
         return replaceData
     }
+
+
+    fun solution2(new_id: String) = new_id.toLowerCase(Locale.getDefault())
+        .filter { it.isLowerCase() || it.isDigit() || it == '-' || it == '_' || it == '.' }
+        .replace("[.]*[.]".toRegex(), ".")
+        .removePrefix(".").removeSuffix(".")
+        .let { if (it.isEmpty()) "a" else it }
+        .let { if (it.length > 15) it.substring(0 until 15) else it }.removeSuffix(".")
+        .let {
+            if (it.length <= 2)
+                StringBuilder(it).run {
+                    while (length < 3) append(it.last())
+                    toString()
+                }
+            else it
+        }
 }
